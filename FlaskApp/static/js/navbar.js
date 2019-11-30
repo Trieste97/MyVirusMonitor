b6e7665860d7776e65417a460e1e68cf39bf44c4
Trieste97 <trsgpp97c29a326r@studenti.unical.it>
2019-11-30 16:27:34 +0100

    external navbar

---
+++ FlaskApp/static/js/navbar.js
@@ -0,0 +1,7 @@
+var navbar = "";
+navbar += "<nav class=\"navbar navbar-expand-sm bg-dark navbar-dark sticky-top\">";
+navbar += "<a class=\"navbar-brand font-weight-bold\" href=\"/home\">Lista files</a>";
+navbar += "<a class=\"navbar-brand font-weight-bold\" href=\"/antivirus\">Statistiche Antivirus</a>";
+navbar += "</nav>";
+
+$("body").prepend(navbar);
